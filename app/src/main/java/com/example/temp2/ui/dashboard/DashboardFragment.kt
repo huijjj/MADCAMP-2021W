d26beaf7bfc9447722d0ac70f8027320d6726ddb
minizzang <79495204+minizzang@users.noreply.github.com>
2021-12-28 20:43:41 +0900

    Add:navigation tabs and icons

--- app/src/main/java/com/example/temp2/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/temp2/ui/dashboard/DashboardFragment.kt
@@ -31,10 +31,10 @@ class DashboardFragment : Fragment() {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+//        val textView: TextView = binding.textDashboard
+//        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
         return root
     }
 
--- app/src/main/java/com/example/temp2/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/temp2/ui/notifications/NotificationsFragment.kt
@@ -31,10 +31,10 @@ class NotificationsFragment : Fragment() {
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+//        val textView: TextView = binding.textNotifications
+//        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
         return root
     }
 
