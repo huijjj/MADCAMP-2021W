1d7fe29eaaa0db2d97020e2a91e6b44eda9f535a
minizzang <79495204+minizzang@users.noreply.github.com>
2021-12-29 22:32:56 +0900

    WIP(squashed!)

---
+++ app/src/main/java/com/example/temp2/Photos.kt
@@ -0,0 +1,4 @@
+package com.example.temp2
+
+class Photos (val name: String, val date: String){
+}
---
+++ app/src/main/java/com/example/temp2/ui/dashboard/PhotoAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.temp2.ui.dashboard
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.temp2.Photos
+import com.example.temp2.R
+import java.util.*
+import kotlin.collections.ArrayList
+
+class PhotoAdapter (val photoList: ArrayList<Photos>) : RecyclerView.Adapter<PhotoAdapter.CustomViewHolder> ()
+{
+    private lateinit var context: Context
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.photo_item, parent, false)
+        context = parent.context
+        return CustomViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+//        val resourceID = context.resources.getIdentifier(photoList.get(position).name, "images", context.packageName)
+//        holder.photo.setImageResource(photoList.get(position).name)
+        val rscId = context.resources.getIdentifier(photoList[position].name, "drawable", context.packageName)
+        holder.photo.setImageResource(rscId)
+//        holder.photo.setImageResource(R.drawable.ic_notifications_black_24dp)
+    }
+
+    override fun getItemCount(): Int {
+        return photoList.size
+    }
+
+    class CustomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val photo:ImageView = itemView.findViewById(R.id.iv_photo)
+    }
+
+}
