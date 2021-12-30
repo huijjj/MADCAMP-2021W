8a839bae08a203928ea73657c20072d81ae67fb1
huijjj <hui0213@postech.ac.kr>
2021-12-30 15:25:13 +0900

    fix: now image is loaded using glide

--- app/src/main/java/com/example/temp2/ui/dashboard/PhotoAdapter.kt
+++ app/src/main/java/com/example/temp2/ui/dashboard/PhotoAdapter.kt
@@ -10,6 +10,7 @@ import com.example.temp2.Photos
 import com.example.temp2.R
 import java.util.*
 import kotlin.collections.ArrayList
+import com.bumptech.glide.Glide
 
 class PhotoAdapter (val photoList: ArrayList<Photos>) : RecyclerView.Adapter<PhotoAdapter.CustomViewHolder> ()
 {
@@ -24,8 +25,9 @@ class PhotoAdapter (val photoList: ArrayList<Photos>) : RecyclerView.Adapter<Pho
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
 //        val resourceID = context.resources.getIdentifier(photoList.get(position).name, "images", context.packageName)
 //        holder.photo.setImageResource(photoList.get(position).name)
-        val rscId = context.resources.getIdentifier(photoList[position].name, "drawable", context.packageName)
-        holder.photo.setImageResource(rscId)
+        val rscId = context.resources.getIdentifier(photoList[position].name, "drawable", context.packageName) //uri
+        Glide.with(context).load(rscId).into(holder.photo)
+//        holder.photo.setImageResource(rscId)
 //        holder.photo.setImageResource(R.drawable.ic_notifications_black_24dp)
     }
 
