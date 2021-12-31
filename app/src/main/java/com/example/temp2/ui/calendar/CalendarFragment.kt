6b1262e0e9815fc3a92fc4184bbcd1294fd32297
huijjj <hui0213@postech.ac.kr>
2021-12-31 18:04:09 +0900

    improve: image loading optimization

--- app/src/main/java/com/example/temp2/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/com/example/temp2/ui/calendar/CalendarFragment.kt
@@ -12,7 +12,7 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.temp2.R
 import com.example.temp2.databinding.FragmentCalendarBinding
-
+import com.bumptech.glide.Glide
 
 
 class CalendarFragment : Fragment() {
@@ -35,122 +35,122 @@ class CalendarFragment : Fragment() {
 
         when(day) {
             1 -> {
-                modalIv.setImageResource(R.drawable.day1)
+                Glide.with(requireContext()).load(R.drawable.day1).into(modalIv)
                 modalTv.text = "“인류의 모든 성과는 시행착오로부터 이루어졌다.” \n\n - 장병규 의장님"
             }
 
             2 -> {
-                modalIv.setImageResource(R.drawable.day2)
+                Glide.with(requireContext()).load(R.drawable.day2).into(modalIv)
                 modalTv.text = "\n20211202는\n거꾸로해도\n20211202입니다.\n"
             }
 
             3 -> {
-                modalIv.setImageResource(R.drawable.day3)
+                Glide.with(requireContext()).load(R.drawable.day3).into(modalIv)
                 modalTv.text = "\n3분반 정희종의 생일은\n 12월 3일로 포항공대의\n개교기념일과 같습니다.\n"
             }
 
             4 -> {
-                modalIv.setImageResource(R.drawable.day4)
+                Glide.with(requireContext()).load(R.drawable.day4).into(modalIv)
                 modalTv.text = "“부디 내 말을 믿어보세요.\n아무리 현실이 답답하더라도 내일은 오늘보다 멋진 날이 되리라, 하고요.” \n\n - 나미야 잡화점의 기적 中"
             }
 
             5 -> {
-                modalIv.setImageResource(R.drawable.day5)
+                Glide.with(requireContext()).load(R.drawable.day5).into(modalIv)
                 modalTv.text = "넌센스 퀴-즈!\nQ. 뽑으면 우는 식물은?\n\nA. 우엉~~ 우엉엉"
             }
 
             6 -> {
-                modalIv.setImageResource(R.drawable.day6)
+                Glide.with(requireContext()).load(R.drawable.day6).into(modalIv)
                 modalTv.text = "쩝쩝박사 김민희 선정\n12월에 꼭 먹어야 할 음식 BEST 5 \n 동치미, 설렁탕, 귤,\n 딸기, 시루떡"
             }
 
             7 -> {
-                modalIv.setImageResource(R.drawable.day7)
+                Glide.with(requireContext()).load(R.drawable.day7).into(modalIv)
                 modalTv.text = "알고 계셨나요?\n눅눅해진 김을 전자레인지에 살짝만 돌려주면 다시\n바삭바삭해진답니다!"
             }
 
             8 -> {
-                modalIv.setImageResource(R.drawable.day8)
+                Glide.with(requireContext()).load(R.drawable.day8).into(modalIv)
                 modalTv.text = "\n\n어쩔티비~\n\n"
             }
 
             9 -> {
-                modalIv.setImageResource(R.drawable.day9)
+                Glide.with(requireContext()).load(R.drawable.day9).into(modalIv)
                 modalTv.text = "\n오늘의 운세 \n\n행운이 찾아온다.\n"
             }
 
             10 -> {
-                modalIv.setImageResource(R.drawable.day10)
+                Glide.with(requireContext()).load(R.drawable.day10).into(modalIv)
                 modalTv.text = "12월 10일은\n 세계 인권의 날입니다.\n1948년 12월 10일 제3차\n국제연합총회에서\n세계 인권 선언을\n선포했습니다."
             }
 
             11 -> {
-                modalIv.setImageResource(R.drawable.day11)
+                Glide.with(requireContext()).load(R.drawable.day11).into(modalIv)
                 modalTv.text = "알고 계셨나요?\n몰입캠프는 학생들이 자율적으로 집중개발을 경험하는\n프로그래밍 캠프입니다."
             }
 
             12 -> {
-                modalIv.setImageResource(R.drawable.day12)
+                Glide.with(requireContext()).load(R.drawable.day12).into(modalIv)
                 modalTv.text = "\n\n\n12가 두 개?!\n\n\n"
             }
 
             13 -> {
-                modalIv.setImageResource(R.drawable.day13)
+                Glide.with(requireContext()).load(R.drawable.day13).into(modalIv)
                 modalTv.text = "\n“제 이름은 외자예요.\n외자여서 외로움을\n많이 타죠.\n하지만 걱정하지 말아요. 사랑에는 공격적이니까”\n"
             }
 
             14 -> {
-                modalIv.setImageResource(R.drawable.day14)
+                Glide.with(requireContext()).load(R.drawable.day14).into(modalIv)
                 modalTv.text = "빙-빙- 돌아가는\n회전목마처럼~\n영원히 계속될 것처럼~\n빙-빙- 돌아온\n우리의 시간처럼~↗\n인생은 회전목마~"
             }
 
             15 -> {
-                modalIv.setImageResource(R.drawable.day15)
+                Glide.with(requireContext()).load(R.drawable.day15).into(modalIv)
                 modalTv.text = "“꿈을 이루고자 하는\n용기만 있다면\n모든 꿈을\n이룰 수 있다.” \n\n- 월트 디즈니"
             }
 
             16 -> {
-                modalIv.setImageResource(R.drawable.day16)
+                Glide.with(requireContext()).load(R.drawable.day16).into(modalIv)
                 modalTv.text = "\n\n저쩔가스레인지~\n\n"
             }
 
             17 -> {
-                modalIv.setImageResource(R.drawable.day17)
+                Glide.with(requireContext()).load(R.drawable.day17).into(modalIv)
                 modalTv.text = "\n알고 계셨나요? \n타조의 안구는 뇌보다\n크다고 합니다!\n\n띠용~\n"
             }
 
             18 -> {
-                modalIv.setImageResource(R.drawable.day18)
+                Glide.with(requireContext()).load(R.drawable.day18).into(modalIv)
                 modalTv.text = "\n짐이 주차를 하면 무엇이냐!\n\n파킹이옵니다 즈언하~\n"
             }
 
             19 -> {
-                modalIv.setImageResource(R.drawable.day19)
+                Glide.with(requireContext()).load(R.drawable.day19).into(modalIv)
                 modalTv.text = "\n오늘의 운세\n\n급하다고 하여 조급하게\n 서두르거나 당황하면\n 손해를 본다.\n"
             }
 
             20 -> {
-                modalIv.setImageResource(R.drawable.day20)
+                Glide.with(requireContext()).load(R.drawable.day20).into(modalIv)
                 modalTv.text = "\n알고 계셨나요?\nTV 동물농장은\n2020년 12월 20일로\n1000회를 맞았습니다.\n"
             }
 
             21 -> {
-                modalIv.setImageResource(R.drawable.day21)
+                Glide.with(requireContext()).load(R.drawable.day21).into(modalIv)
                 modalTv.text = "“건전한 열등감이란 타인과 비교해서 생기는 것이 아니라 ‘이상적인 나’와 비교해서 생기는 것이라네.”\n\n- 미움받을용기 中"
             }
 
             22 -> {
-                modalIv.setImageResource(R.drawable.day22)
+                Glide.with(requireContext()).load(R.drawable.day22).into(modalIv)
                 modalTv.text = "1977년 12월 22일은\n‘수출 100억불의 날’로,\n대한민국 수출액이\n100억 달러를\n돌파함과 동시에\nGDP 1,000달러를\n돌파했습니다."
             }
 
             23 -> {
-                modalIv.setImageResource(R.drawable.day23)
+                Glide.with(requireContext()).load(R.drawable.day23).into(modalIv)
                 modalTv.text = "\n“잘자요. 꼬마 아가씨. 오늘은 좋은 꿈 꾸기로 약속!”\n"
             }
 
             24 -> {
-                modalIv.setImageResource(R.drawable.day24)
+                Glide.with(requireContext()).load(R.drawable.day24).into(modalIv)
                 modalTv.text = "\n\n\nHappy christmas eve!\n\n\n"
             }
 
@@ -161,30 +161,30 @@ class CalendarFragment : Fragment() {
             Toast.makeText(context, "짜잔!", Toast.LENGTH_SHORT).show()
 
             when(day) {
-                1 -> binding.decoButton1.setImageResource(R.drawable.deco_open_1)
-                2 -> binding.decoButton2.setImageResource(R.drawable.deco_open_2)
-                3 -> binding.decoButton3.setImageResource(R.drawable.deco_open_3)
-                4 -> binding.decoButton4.setImageResource(R.drawable.deco_open_4)
-                5 -> binding.decoButton5.setImageResource(R.drawable.deco_open_5)
-                6 -> binding.decoButton6.setImageResource(R.drawable.deco_open_6)
-                7 -> binding.decoButton7.setImageResource(R.drawable.deco_open_7)
-                8 -> binding.decoButton8.setImageResource(R.drawable.deco_open_8)
-                9 -> binding.decoButton9.setImageResource(R.drawable.deco_open_9)
-                10 -> binding.decoButton10.setImageResource(R.drawable.deco_open_10)
-                11 -> binding.decoButton11.setImageResource(R.drawable.deco_open_11)
-                12 -> binding.decoButton12.setImageResource(R.drawable.deco_open_12)
-                13 -> binding.decoButton13.setImageResource(R.drawable.deco_open_13)
-                14 -> binding.decoButton14.setImageResource(R.drawable.deco_open_14)
-                15 -> binding.decoButton15.setImageResource(R.drawable.deco_open_15)
-                16 -> binding.decoButton16.setImageResource(R.drawable.deco_open_16)
-                17 -> binding.decoButton17.setImageResource(R.drawable.deco_open_17)
-                18 -> binding.decoButton18.setImageResource(R.drawable.deco_open_18)
-                19 -> binding.decoButton19.setImageResource(R.drawable.deco_open_19)
-                20 -> binding.decoButton20.setImageResource(R.drawable.deco_open_20)
-                21 -> binding.decoButton21.setImageResource(R.drawable.deco_open_21)
-                22 -> binding.decoButton22.setImageResource(R.drawable.deco_open_22)
-                23 -> binding.decoButton23.setImageResource(R.drawable.deco_open_23)
-                24 -> binding.decoButton24.setImageResource(R.drawable.deco_open_24)
+                1 -> Glide.with(requireContext()).load(R.drawable.deco_open_1).into(binding.decoButton1)
+                2 -> Glide.with(requireContext()).load(R.drawable.deco_open_2).into(binding.decoButton2)
+                3 -> Glide.with(requireContext()).load(R.drawable.deco_open_3).into(binding.decoButton3)
+                4 -> Glide.with(requireContext()).load(R.drawable.deco_open_4).into(binding.decoButton4)
+                5 -> Glide.with(requireContext()).load(R.drawable.deco_open_5).into(binding.decoButton5)
+                6 -> Glide.with(requireContext()).load(R.drawable.deco_open_6).into(binding.decoButton6)
+                7 -> Glide.with(requireContext()).load(R.drawable.deco_open_7).into(binding.decoButton7)
+                8 -> Glide.with(requireContext()).load(R.drawable.deco_open_8).into(binding.decoButton8)
+                9 -> Glide.with(requireContext()).load(R.drawable.deco_open_9).into(binding.decoButton9)
+                10 -> Glide.with(requireContext()).load(R.drawable.deco_open_10).into(binding.decoButton10)
+                11 -> Glide.with(requireContext()).load(R.drawable.deco_open_11).into(binding.decoButton11)
+                12 -> Glide.with(requireContext()).load(R.drawable.deco_open_12).into(binding.decoButton12)
+                13 -> Glide.with(requireContext()).load(R.drawable.deco_open_13).into(binding.decoButton13)
+                14 -> Glide.with(requireContext()).load(R.drawable.deco_open_14).into(binding.decoButton14)
+                15 -> Glide.with(requireContext()).load(R.drawable.deco_open_15).into(binding.decoButton15)
+                16 -> Glide.with(requireContext()).load(R.drawable.deco_open_16).into(binding.decoButton16)
+                17 -> Glide.with(requireContext()).load(R.drawable.deco_open_17).into(binding.decoButton17)
+                18 -> Glide.with(requireContext()).load(R.drawable.deco_open_18).into(binding.decoButton18)
+                19 -> Glide.with(requireContext()).load(R.drawable.deco_open_19).into(binding.decoButton19)
+                20 -> Glide.with(requireContext()).load(R.drawable.deco_open_20).into(binding.decoButton20)
+                21 -> Glide.with(requireContext()).load(R.drawable.deco_open_21).into(binding.decoButton21)
+                22 -> Glide.with(requireContext()).load(R.drawable.deco_open_22).into(binding.decoButton22)
+                23 -> Glide.with(requireContext()).load(R.drawable.deco_open_23).into(binding.decoButton23)
+                24 -> Glide.with(requireContext()).load(R.drawable.deco_open_24).into(binding.decoButton24)
                 else -> null
             }
         }.setNegativeButton("열지않기", null).create()
