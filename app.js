4f9b52faa91ee9ceac4b6b2b3eec17509343073f
huijjj <hui0213@postech.ac.kr>
2022-01-06 04:41:18 +0000

    add: socket.io

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const app = require('express')();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
 
 app.get('/', (req, res) => {
-    res.send("Hello World!");
-})
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('request_message', (msg) => {
+        io.emit('response_message', msg);
+    });
+    
+    socket.on('disconnect', async () => {
+        console.log('user disconnected');
+    });
+});
 
 http.listen(80, () => {
     console.log('connected at 80');
