29a21bd7d330f2fd55c3c451f2274765e3e17500
huijjj <hui0213@postech.ac.kr>
2022-01-08 15:23:31 +0000

    add: DB api

--- app.js
+++ app.js
@@ -1,10 +1,102 @@
-const { request } = require('http');
-
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const res = require('express/lib/response');
 
+const PORT = 443;
 let roomSet = new Map();
+
+app.use(express.json());
+
+// db setting
+const mysql = require('mysql');
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'express_db'
+});
+
+con.connect(function(err) {
+    if(err) {
+        throw err;
+    }
+    console.log("db connected");
+    // const sql = "SELECT * FROM users";
+    // con.query(sql, function(err, result) {
+    //     if(err) {
+    //         throw err;
+    //     }
+    //     else {
+    //         console.log(result);
+    //     }
+    // });
+});
+
+// creating db (allowed only once)
+// const sql = 'CREATE DATABASE express_db
+
+// query for creating table, id | name | email | 
+// const sql = 'CREATE TABLE users (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, win INT DEFAULT 0, lose INT DEFAULT 0)';
+
+// query for searching all users
+// const sql = "SELECT * FROM users";
+
+// query for inserting user
+// const sql = "INSERT INTO users(name, email, win, lose) VALUES(?, ?, ?, ?)";
+        
+// for checking if user with given email already exists in DB
+app.get('/user/:email', (req, res) => {
+    console.log(`GET /isUser/${req.params.email}`);
+    const sql = `SELECT * FROM users WHERE email='${req.params.email}'`;
+    con.query(sql, function(err, result) {
+        if (err) {
+            throw err;
+        }
+        res.send(result); // sends empty list if user is not a member [] [{id: 1, name: "정희종", ... }]
+    });
+});
+
+// for adding new user in DB
+app.post('/user', (req, res) => {
+    const name = String(req.body.name);
+    const email = String(req.body.email);
+    console.log(`POST /user { ${name} ${email} }`);
+    const sql = "INSERT INTO users(name, email, win, lose) VALUES(?, ?, ?, ?)";
+    con.query(sql, [ name, email, 0, 0 ], function(err, result) {
+        if(err) {
+            throw err;
+        }
+        res.send(result);
+    });
+});
+
+// for updating user win count
+app.patch('/change/:email/win/:new', (req, res) => {
+    const email = String(req.params.email);
+    const new_win = Number(req.params.new);
+    const sql = `UPDATE users SET win=${new_win} WHERE email='${email}'`;
+    con.query(sql, function(err, result) {
+        if(err) {
+            throw err;
+        }
+        res.send(result);
+    });
+});
+
+// for updating user lose count
+app.patch('/change/:email/lose/:new', (req, res) => {
+    const email = String(req.params.email);
+    const new_lose = Number(req.params.new);
+    const sql = `UPDATE users SET lose=${new_lose} WHERE email='${email}'`;
+    con.query(sql, function(err, result) {
+        if(err) {
+            throw err;
+        }
+        res.send(result);
+    });
+});
 
 io.on('connection', (socket) => {
     console.log('(connection) ', socket.id);
@@ -63,7 +155,6 @@ io.on('connection', (socket) => {
     });
 });
 
-
-http.listen(443, () => {
-    console.log('listening to port 443');
+http.listen(PORT, () => {
+    console.log(`listening to port ${PORT}`);
 });
