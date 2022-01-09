7884ad8e12f71b570c227e753cae29864c367b05
huijjj <hui0213@postech.ac.kr>
2022-01-09 12:37:15 +0000

    add: api request, win rate update

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const res = require('express/lib/response');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 
 const PORT = 443;
 let roomSet = new Map();
 
-app.use(express.json());
 
 // db setting
 const mysql = require('mysql');
@@ -46,54 +48,33 @@ con.connect(function(err) {
 // query for inserting user
 // const sql = "INSERT INTO users(name, email, win, lose) VALUES(?, ?, ?, ?)";
         
-// for checking if user with given email already exists in DB
-app.get('/user/:email', (req, res) => {
-    console.log(`GET /isUser/${req.params.email}`);
-    const sql = `SELECT * FROM users WHERE email='${req.params.email}'`;
+// for checking if user with given kakao id already exists in DB
+app.get('/users/:kid', (req, res) => {
+    console.log(`GET /isUser/${req.params.kid}`);
+    const sql = `SELECT * FROM users WHERE id='${req.params.kid}'`;
     con.query(sql, function(err, result) {
         if (err) {
             throw err;
         }
-        res.send(result); // sends empty list if user is not a member [] [{id: 1, name: "정희종", ... }]
+        console.log(result);
+        res.json({ user : result }); // sends empty list if user is not a member [] [{id: 1, name: "정희종", ... }]
     });
 });
 
 // for adding new user in DB
-app.post('/user', (req, res) => {
+app.post('/users', (req, res) => {
+    // console.log(req.body);
+    // console.log(req.params);
+    // console.log(req.query);
+    const kid = Number(req.body.kid);
     const name = String(req.body.name);
-    const email = String(req.body.email);
-    console.log(`POST /user { ${name} ${email} }`);
-    const sql = "INSERT INTO users(name, email, win, lose) VALUES(?, ?, ?, ?)";
-    con.query(sql, [ name, email, 0, 0 ], function(err, result) {
+    console.log(`POST /user { ${kid} ${name} }`);
+    const sql = "INSERT INTO users(id, name, win, lose) VALUES(?, ?, ?, ?)";
+    con.query(sql, [ kid, name, 0, 0 ], function(err, result) {
         if(err) {
             throw err;
         }
-        res.send(result);
-    });
-});
-
-// for updating user win count
-app.patch('/change/:email/win/:new', (req, res) => {
-    const email = String(req.params.email);
-    const new_win = Number(req.params.new);
-    const sql = `UPDATE users SET win=${new_win} WHERE email='${email}'`;
-    con.query(sql, function(err, result) {
-        if(err) {
-            throw err;
-        }
-        res.send(result);
-    });
-});
-
-// for updating user lose count
-app.patch('/change/:email/lose/:new', (req, res) => {
-    const email = String(req.params.email);
-    const new_lose = Number(req.params.new);
-    const sql = `UPDATE users SET lose=${new_lose} WHERE email='${email}'`;
-    con.query(sql, function(err, result) {
-        if(err) {
-            throw err;
-        }
+        console.log(result);
         res.send(result);
     });
 });
@@ -101,15 +82,15 @@ app.patch('/change/:email/lose/:new', (req, res) => {
 io.on('connection', (socket) => {
     console.log('(connection) ', socket.id);
     
-    socket.on('join', (room, name) => {
-        console.log("(join)", room, socket.id);
+    socket.on('join', (room, name, kid) => {
+        console.log("(join)", room, name, kid, socket.id);
         
         if(roomSet.has(room)) { // this room already exist
             if(roomSet.get(room).length == 1) { // able to join
-                roomSet.set(room, [ roomSet.get(room)[0], { id: socket.id, name: name} ]);
+                roomSet.set(room, [ roomSet.get(room)[0], { id: socket.id, name: name, kid: kid } ]);
                 socket.join(room);
-                io.to(roomSet.get(room)[0].id).emit('start', "white", roomSet.get(room)[1].name); // color, opponent name
-                io.to(roomSet.get(room)[1].id).emit('start', "black", roomSet.get(room)[0].name);
+                io.to(roomSet.get(room)[0].id).emit('start', "black", roomSet.get(room)[1].name);
+                io.to(roomSet.get(room)[1].id).emit('start', "white", roomSet.get(room)[0].name); // color, opponent name
             }
             else { // room already occupied
                 // send error msg
@@ -119,13 +100,23 @@ io.on('connection', (socket) => {
         else {  // room does not exist
             // create and join
             socket.join(room);
-            roomSet.set(room, [{ id: socket.id, name: name }]);
+            roomSet.set(room, [{ id: socket.id, name: name, kid: kid }]);
         }
     });
 
     socket.on('rejoin', (room) => {
         // client re-joins the room after activity change
         socket.join(room);
+    });
+
+    socket.on('leave', (room) => {
+        console.log("(leave)", room, socket.id);
+        socket.leave(room);
+        if(roomSet.has(room)) {
+            if(roomSet.get(room).length == 1) {
+                roomSet.delete(room);
+            }
+        }
     });
 
     socket.on('set go', (room, color, X, Y) => {
@@ -137,6 +128,46 @@ io.on('connection', (socket) => {
         console.log("(game end)", room, winner);
         io.to(room).emit('game result', winner);
         roomSet.delete(room);
+    });
+
+    socket.on('win', (kid) => {
+        // get users win data
+        const get_sql = `SELECT * FROM users WHERE id='${kid}'`;
+        con.query(get_sql, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            if(result.length == 1) {
+                const win = Number(result[0].win);
+                const patch_sql = `UPDATE users SET win=${win + 1} WHERE id='${kid}'`;
+                con.query(patch_sql, function(err, _) {
+                    if(err) {
+                        throw err;
+                    }
+                    console.log("user ", kid, " updated");
+                });
+            }
+        });
+    });
+
+    socket.on('lose', (kid) => {
+        // get users win data
+        const get_sql = `SELECT * FROM users WHERE id='${kid}'`;
+        con.query(get_sql, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            if(result.length == 1) {
+                const lose = Number(result[0].lose);
+                const patch_sql = `UPDATE users SET lose=${lose + 1} WHERE id='${kid}'`;
+                con.query(patch_sql, function(err, _) {
+                    if(err) {
+                        throw err;
+                    }
+                    console.log("user ", kid, " updated");
+                });
+            }
+        });
     });
 
     socket.on('rooms', () => {
