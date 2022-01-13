95ed10db676acf6d528819bd418c4798160240f1
SeungjaeLim <seungjaelim@kaist.ac.kr>
2022-01-13 10:01:22 +0000

    feat: create random id key

--- api.js
+++ api.js
@@ -5,6 +5,21 @@ const connection = mysql.createConnection(dbconfig);
 
 const app = express();
 
+function CreateRandomID()
+{
+  let dateKey = new Date();
+  let jobKey = '';
+  var randKey = Math.floor(Math.random()*10);
+  let month = dateKey.getMonth() % 10;
+  let day = dateKey.getDate() + 10;
+  let hours = dateKey.getHours() + 10;
+  let minutes = dateKey.getMinutes() + 10;
+  let seconds = dateKey.getSeconds() + 10;
+  jobKey = jobKey + randKey + month + day + hours + minutes + seconds;
+  console.log('jobKey: ' + jobKey);
+  return jobKey;
+}
+
 app.set('port', process.env.PORT || 443);
 
 app.get('/', (req, res) => {
