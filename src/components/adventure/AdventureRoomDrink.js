b5829c9222cccc130d10caf29fcad0dce5b30cb0
Giyeong Kim <ggcc503@gmail.com>
2022-01-14 11:44:15 +0000

    feat: add get point in game bar

--- src/components/adventure/AdventureRoomDrink.js
+++ src/components/adventure/AdventureRoomDrink.js
@@ -1,9 +1,55 @@
 import Button from '@mui/material/Button';
+import { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import AdventureRoomFinal from './AdventureRoomFinal';
 import GameBar from './GameBar';
 
 export default function AdventureRoomDrink({ floor, stamina, setStamina, setFloor, setIsInGame }) {
+  const difficultyList = [
+    {
+      stamina: 100,
+      speed: 1
+    },
+    {
+      stamina: 100,
+      speed: 1.2
+    },
+    {
+      stamina: 100,
+      speed: 1.4
+    },
+    {
+      stamina: 150,
+      speed: 1.4
+    },
+    {
+      stamina: 150,
+      speed: 1.6
+    },
+    {
+      stamina: 170,
+      speed: 1.7
+    },
+    {
+      stamina: 200,
+      speed: 1.8
+    },
+    {
+      stamina: 220,
+      speed: 2
+    },
+    {
+      stamina: 250,
+      speed: 2
+    },
+    {
+      stamina: 300,
+      speed: 2
+    }
+  ]
+
+  const [hStamina, setHStamina] = useState(difficultyList[floor-1].stamina);
+  
 
   const handleNext = () => {
     setFloor(floor+1);
@@ -16,8 +62,11 @@ export default function AdventureRoomDrink({ floor, stamina, setStamina, setFloo
       <h1>
         현지랑 술 배틀
       </h1>
+      <h3>
+        현지 체력: {hStamina}
+      </h3>
       <div>
-        <GameBar />
+        <GameBar speed={difficultyList[floor-1].speed } setHStamina={setHStamina}/>
       </div>
       <Button onClick={handleNext}>
         {(floor===10) ? "마지막" : floor + 1}층으로 이동하기
--- src/components/adventure/GameBar.js
+++ src/components/adventure/GameBar.js
@@ -1,14 +1,29 @@
 import { Button } from "@mui/material";
 import { useEffect, useRef, useState } from "react";
 
-export default function GameBar() {
+export default function GameBar({ isStart, speed, setHStamina }) {
   
+  // const [isAttack, setIsAttack] = useState(false);
+  // const [stoppedX, setStoppedX] = useState(0);
+  var stoppedX = Number(0);
+  var isAttack = false;
+  var attackValue = 0;
+  var interval;
+ 
   var x = 0;
+  
+  // function block(ctx, x) {
+  //   ctx.beginPath();
+  //   ctx.rect(x, 0, 10, 50);
+  //   ctx.fillStyle = "black";
+  //   ctx.fill();
+  //   ctx.closePath();
+  // }
 
   var canvasRef = useRef(null);
-
   const draw = ()=> {
-    
+    if (x > 405) return;
+
     var canvas = canvasRef.current;
     var ctx = canvas.getContext("2d");
     
@@ -36,18 +51,46 @@ export default function GameBar() {
     ctx.fillStyle = "black";
     ctx.fill();
     ctx.closePath();
-
-    x += 1.5;
+    
+    stoppedX = x;
+    x += speed;
   };
 
-  var interval = setInterval(draw, 0.01);
-  
+  const handleAttack = () => {
+    if (315 <= stoppedX && stoppedX <= 335) {
+      isAttack = true;
+      attackValue = 50;
+      console.log("red");
+    }
+    else if (300 <= stoppedX && stoppedX < 315 || 335 < stoppedX && stoppedX <= 350) {
+        isAttack = true;
+        attackValue = 30;
+        console.log("orange");
+    }
+    else {
+      attackValue = 30;
+      console.log("outside");
+    }
+  }
 
+  
   return (
     <>
-      <canvas ref={canvasRef} width={400} height={50}></canvas>
-      <Button onClick={() => clearInterval(interval)}>
-        공격하기
+      <Button onClick={() => {
+        interval = setInterval(draw, 0.01);
+      }}>
+        싸우기
+      </Button>
+      <canvas ref={canvasRef} width={400} height={50}></canvas><br/>
+      <Button onClick={() => {
+        
+        clearInterval(interval);
+        handleAttack();
+        console.log(`stoppedX = ${stoppedX}`);
+        console.log(`attackValue = ${attackValue}`)
+        }}
+      >
+        멈추기
       </Button>
     </>
   );
