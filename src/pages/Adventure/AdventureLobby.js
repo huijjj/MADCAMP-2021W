59f08833595acc6da93ac0ae86b797b91b502d05
Giyeong Kim <ggcc503@gmail.com>
2022-01-16 05:10:24 +0000

    feat: add list for card at adventure lobby

--- src/pages/Adventure/AdventureLobby.js
+++ src/pages/Adventure/AdventureLobby.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react"
 import { useNavigate } from 'react-router-dom';
 
 import Card from '@mui/material/Card';
@@ -14,6 +14,15 @@ import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import HomeIcon from '@mui/icons-material/Home';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Slide from '@mui/material/Slide';
+
+
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 export default function AdventureLobby({ animalList, setIsLobby, setAnimal, setFloor, stamina, setStamina }) {
   const navigate = useNavigate();
@@ -41,55 +50,59 @@ export default function AdventureLobby({ animalList, setIsLobby, setAnimal, setF
   return (
     <div id = "adventure-lobby">
       <HomeIcon onClick={() => navigate(-1)} />
-      { animalList?.map(
-          (animal, index) =>
-            <Card key={index} sx={{ maxWidth: 345 }}>
-              <CardMedia 
-                component="img"
-                image={`/images/${animal.type}.png`}
-                alt={`${animal.type}.png`}
-              />
-              <CardContent> 
-                <Typography gutterBottom variant="h5" component="div">
-                  {animal.name}, {animal.sex}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  지: {animal.geee}<br/>
-                  덕: {animal.duck}<br/>
-                  체: {animal.chae}<br/>
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small" onClick={() => handleClickExit(index)}>
-                참가하기
-                </Button>
+      <List>
+        {animalList?.map(
+            (animal, index) =>
+              <ListItem>
+              <Card key={index} sx={{ maxWidth: 345 }}>
+                <CardMedia 
+                  component="img"
+                  image={`/images/${animal.type}.png`}
+                  alt={`${animal.type}.png`}
+                />
+                <CardContent> 
+                  <Typography gutterBottom variant="h5" component="div">
+                    {animal.name}, {animal.sex}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    지: {animal.geee}<br/>
+                    덕: {animal.duck}<br/>
+                    체: {animal.chae}<br/>
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" onClick={() => handleClickExit(index)}>
+                  참가하기
+                  </Button>
 
-                <Dialog
-                  open={exitOpen}
-                  onClose={handleCloseNo}
-                  aria-labelledby="alert-dialog-title"
-                  aria-describedby="alert-dialog-description"
-                >
-                  <DialogTitle id="alert-dialog-title">
-                    {animalList[animalIndex].name}와(과) 함께 탐험을 시작 하시겠습니까?
-                  </DialogTitle>
+                  <Dialog
+                    open={exitOpen}
+                    onClose={handleCloseNo}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                  >
+                    <DialogTitle id="alert-dialog-title">
+                      {animalList[animalIndex].name}와(과) 함께 탐험을 시작 하시겠습니까?
+                    </DialogTitle>
 
-                  <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                      탐험중에는 동물을 변경할 수 없습니다<br/>
-                      (체력이 0 보다 낮아지면 동물이 죽을수도 있습니다)
-                    </DialogContentText>
-                  </DialogContent>
+                    <DialogContent>
+                      <DialogContentText id="alert-dialog-description">
+                        탐험중에는 동물을 변경할 수 없습니다<br/>
+                        (체력이 0 보다 낮아지면 동물이 죽을수도 있습니다)
+                      </DialogContentText>
+                    </DialogContent>
 
-                  <DialogActions>
-                    <Button onClick={handleCloseNo}>아니요</Button>
-                    <Button onClick={() => handleCloseYes(animalIndex)}>네</Button>
-                  </DialogActions>
-                </Dialog>
-              </CardActions>
-            </Card>
-        )
-      }
+                    <DialogActions>
+                      <Button onClick={handleCloseNo}>아니요</Button>
+                      <Button onClick={() => handleCloseYes(animalIndex)}>네</Button>
+                    </DialogActions>
+                  </Dialog>
+                </CardActions>
+              </Card>
+              </ListItem>
+          )
+        }
+      </List>
       
     </div>  
   );
