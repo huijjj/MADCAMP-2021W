ccf3409255f0f646efafe3c88225d6d2e7f9fe35
SeungjaeLim <seungjaelim@kaist.ac.kr>
2022-01-16 21:11:30 +0000

    chore: re-sort

--- api.js
+++ api.js
@@ -474,13 +474,13 @@ app.get('/api/item/owner/:ownerId', (req, res) => {
   let sqlOwnerItemDumbell = 'SELECT * from Item where owner = ? and type = \'dumbell\'';
   let sqlOwnerItemBook = 'SELECT * from Item where owner = ? and type = \'book\'';
   let paramOwnerItem = [ownerId];
-  connection.query(sqlOwnerItemRose, paramOwnerItem, (error, results) => {
+  connection.query(sqlOwnerItemBook, paramOwnerItem, (error, results) => {
     if (error) throw error;
     inventory.push(results);
-    connection.query(sqlOwnerItemDumbell, paramOwnerItem, (error, results) => {
+    connection.query(sqlOwnerItemRose, paramOwnerItem, (error, results) => {
       if (error) throw error;
       inventory.push(results);
-      connection.query(sqlOwnerItemBook, paramOwnerItem, (error, results) => {
+      connection.query(sqlOwnerItemDumbell, paramOwnerItem, (error, results) => {
         if (error) throw error;
         inventory.push(results);
         console.log('/api/item/owner/'+ownerId);
