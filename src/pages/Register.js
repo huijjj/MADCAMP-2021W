5f99359988cee7af18a5aeb131f595b49c8f7264
huijjj <hui0213@postech.ac.kr>
2022-01-17 10:51:33 +0000

    WIP

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,34 +1,44 @@
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import "../style/Register.css";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogTitle from '@mui/material/DialogTitle';
+import { useState } from 'react';
 
 const API_BASE = process.env.REACT_APP_API_BASE;
 
 
 export default function Register() {
+  const [ alertOpen, setAlertOpen ] = useState(false);
+  const [ alertTitle, setAlertTitle ] = useState("");
+  const [ done, setDone ] = useState(false);
   const navigate = useNavigate();
+
   const onRegister = (e) => {
     e.preventDefault();
+    // console.log(e.target);
     const id = e.target.id.value;
     const nick = e.target.nick.value;
     const pwd = e.target.pwd.value;
-    axios.post(`${API_BASE}/user/register`,
-    {
+    axios.post(`${API_BASE}/user/register`, {
       id: id,
       nick: nick, 
       pwd: pwd
-    })
-    .then(res => {
+    }).then(res => {
       // console.log(res.data.status);
       if(res.data.status) {
-        window.alert("모종의 이유로 회원가입에 실패했습니다.");
+        setAlertOpen(true);
+        setAlertTitle("모종의 이유로 회원가입에 실패했습니다.");
       }
       else {
-        window.alert("회원가입에 성공하셨습니다. 가입하신 계정으로 로그인 해주세요.");
-        navigate(`/`);
+        setAlertOpen(true);
+        setAlertTitle(`회원가입에 성공하셨습니다.`);
+        setDone(true);
       }
-    })
-    .catch(err => console.log(err));
+    }).catch(err => console.log(err));
   }
 
   return (
@@ -37,14 +47,31 @@ export default function Register() {
         <div className="Title">REGISTER</div>
         <form className="LoginForm" onSubmit={onRegister}>
           <div className="LoginFormInput">
-            <input className="LoginFormInputId" autoComplete="off" placeholder="id" name="id"></input>
-            <input className="LoginFormInputId" autoComplete="off" placeholder="password" name="pwd"></input>
-            <input className="LoginFormInputId" autoComplete="off" placeholder="nickname" name="nick"></input>
-            <input autoComplete="off" placeholder="email"></input>
+            <TextField label="ID" style={{ marginBottom: "0.6rem"}} autoComplete="off" size="small" name="id"/>
+            <TextField label="PASSWORD" style={{ marginBottom: "0.6rem"}} autoComplete="off" size="small" name="pwd"/>
+            <TextField label="NICKNAME" style={{ marginBottom: "0.6rem"}} autoComplete="off" size="small" name="nick"/>
+            <TextField label="E-MAIL" autoComplete="off" size="small"/>
           </div>
-          <input type="submit"></input>
+          <input type="submit" className="RegisterSubmitButton" variant="contained" value="REGISTER"></input>
         </form>
+        <div className="RegisterButton" onClick={(e) => {
+          e.preventDefault();
+          navigate("/");
+        }}>
+          계정이 이미 있으신가요?
+        </div>
       </div>
+      <Dialog open={alertOpen}>
+        <DialogTitle style={{ display: "flex", justifyContent: "center" }} >{alertTitle}</DialogTitle>
+        <DialogActions>
+          <Button onClick={() => {
+            if(done) {
+              navigate("/");
+            }
+            setAlertOpen(false);
+          }}>확인</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
