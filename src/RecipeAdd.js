bab6b21bdd8944286e22d09f80e327941afcfdba
huijjj <hui0213@postech.ac.kr>
2022-01-23 08:30:17 +0000

    add: image upload

--- src/RecipeAdd.js
+++ src/RecipeAdd.js
@@ -2,12 +2,15 @@ import { useParams, useNavigate } from "react-router-dom";
 import { useState, useRef } from "react";
 import axios from "axios";
 
+import ImageUploader from "react-images-upload";
+
 const API_BASE = 'http://192.249.18.176:443';
 
 export default function RecipeAdd() {
     const { userId } = useParams();
     const [ ingredients, setIngredients ] = useState([]);
     const [ procedures, setProcedures ] = useState([]);
+    const [ img, setImg ] = useState("");
     const ingredientNameRef = useRef();
     const ingredientAmountRef = useRef();
     const procedureRef = useRef();
@@ -20,6 +23,7 @@ export default function RecipeAdd() {
         e.preventDefault();  
         if(titleRef.current.value) {
             axios.post(`${API_BASE}/recipe`, {
+                img: img,
                 owner: userId,
                 title: titleRef.current.value,
                 memo: recipememoRef.current.value,
@@ -53,12 +57,36 @@ export default function RecipeAdd() {
         setProcedures(procedures.filter((_, idx) => idx !== i));
     }
 
+    const onImageChange = async (e) => {
+        const thumbnail = e[0];
+        const fd = new FormData();
+        fd.append('image', thumbnail);
+
+        await axios.post(`${API_BASE}/image`, fd).then(res => {
+            console.log(res.data);
+            setImg(res.data);
+        }).catch(console.log);
+    }
+
     return (
         <div className = "add_body">
             <div className = "container">
                 <div className = "container_text">레시피 등록</div>
                 <div className = "image_title_memo">
-                    <div id = "add_image">image</div>
+                    <div id = "add_image" className="image_add_container">
+                        {
+                            img === "" ? <></>
+                            :<img src={`${API_BASE}/image/${img}`} style={{ width: "200px", height: "200px" }}/>
+                        }
+                        <ImageUploader
+                            withLabel={false}
+                            withIcon={false}
+                            buttonText="사진 선택"
+                            imgExtension={[".png"]}
+                            maxFileSize={5242880}
+                            singleImage={true}
+                            onChange={onImageChange} />
+                    </div>
                     <div className = "title_memo">
                         <div className = "title_favorite" style={{ display: "flex"}}>
                             <input id = "input_title" placeholder="title" ref={titleRef}></input>
