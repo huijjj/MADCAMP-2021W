56cf39a1e7f1c190b61335d9bc26e976dfb0a96b
huijjj <hui0213@postech.ac.kr>
2022-01-25 22:46:37 +0900

    add: dialog in login

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import './Login.css';
+import Slide from '@mui/material/Slide';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
+import DialogActions from '@mui/material/DialogActions';
+import { SettingsPhoneTwoTone } from '@material-ui/icons';
+
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+  });
 
 function Login() {
     const nav = useNavigate();
+    const [ title, setTitle ] = useState("");
+    const [ login, setLogin ] = useState(false);
+    const [ show, setShow ] = useState(false);
+    const [ nickname, setNickname ] = useState("");
+    const [ userId, setUserId ] = useState("");
+
     function handleClick(e) {
         e.preventDefault();
         
@@ -19,12 +36,20 @@ function Login() {
                 console.log(res.data);
                 console.log(res.data.nickname);
 
-                nav(`/home/${e.target.id.value}`, {state: {nickname: res.data.nickname}});
-                window.alert(`환영합니다 ${res.data.nickname}님!`);
+                setUserId(e.target.id.value);
+                setNickname(res.data.nickname);
+                setTitle(`환영합니다 ${res.data.nickname}님!`);
+                setLogin(true);
+                setShow(true);
+                // nav(`/home/${e.target.id.value}`, {state: {nickname: res.data.nickname}});
+                // window.alert(`환영합니다 ${res.data.nickname}님!`);
             }
         }).catch((e) => {
             console.log(e);
-            window.alert('아이디와 비밀번호를 다시 확인해주세요.');
+            setTitle(`아이디와 비밀번호를 다시 확인해주세요.`);
+            setShow(true);
+            setLogin(false);
+            // window.alert('아이디와 비밀번호를 다시 확인해주세요.');
         });
 
     }
@@ -49,6 +74,25 @@ function Login() {
                 <div className='registerlink' style={{marginTop: "2%"}}><a href="/register">아이디가 없으신가요?</a></div>
             </div>
         </div>
+
+        <Dialog
+            open={show}
+            TransitionComponent={Transition}
+            onClose={() => setLogin(false)}>
+            <DialogTitle>{title}</DialogTitle>
+            <DialogActions>
+                <Button onClick={(e) => {
+                    e.preventDefault();
+                    if(login) {
+                        nav(`/home/${userId}`, {state: {nickname: nickname}});
+                    } else {
+                        setShow(false);
+                    }
+                }}>
+                    확인
+                </Button>
+            </DialogActions>
+        </Dialog>
     </div>
     );
 }
