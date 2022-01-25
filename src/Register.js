e95dfb19d8bdad64081b822930f6e5197725dd70
huijjj <hui0213@postech.ac.kr>
2022-01-25 22:10:45 +0900

    fix: register page

--- src/Register.js
+++ src/Register.js
@@ -30,30 +30,33 @@ function Register() {
   }
   return (
     <div className='mainbackground'>
-        <div className='domain'>회원가입</div>
-        <div className='registerbody'>
-            <form onSubmit={handleClick}>
-                <div className='registerparent'>
-                    <div className='registerchild'> 
-                        <div>
-                            <input className='inputid' name="id" type = "text"  placeholder='Enter User ID' required/>
+        <div style={{width: "35%", height:"45%"}} className='domainstructure'>
+            <div className='domain'>회원가입</div>
+            <div className='registerbody'>
+                <form onSubmit={handleClick}>
+                    <div className='registerparent'>
+                        <div className='registerchild'> 
+                            <div>
+                                <input className='inputid' name="id" type = "text"  placeholder='Enter User ID' required/>
+                            </div>
+                            <div>
+                                <input className='inputid' name="pw" type = "password" placeholder='Enter Password' required/>
+                            </div>
+                            <div>
+                                <input className='inputid' name="rpw" type = "password" placeholder='Enter Password Again' required/>
+                            </div>
+                            <div>
+                                <input className='inputpw' name="nickname" type = "text" placeholder='Enter User Nickname' required/>
+                            </div>
                         </div>
-                        <div>
-                            <input className='inputpw' name="pw" type = "password" placeholder='Enter Password' required/>
-                        </div>
-                        <div>
-                            <input className='inputrpw' name="rpw" type = "password" placeholder='Enter Password Again' required/>
-                        </div>
-                        <div>
-                            <input className='inputnickname' name="nickname" type = "text" placeholder='Enter User Nickname' required/>
-                        </div>
+                        <button className = "registerbutton" type='submit' >회원가입</button>
                     </div>
-                    <button className = "registerbutton" type='submit'>회원가입</button>
-                </div>
-            </form>
+                </form>
+                <div className='loginlink' style={{paddingTop: "30px"}}><a href='/login'>기존 회원이신가요?</a></div>
+            </div>
         </div>
-	</div>
-  );
+    </div>
+    );
 }
 
 export default Register;
